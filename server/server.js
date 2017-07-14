e6952377935aa23c4c932073098274df6b6f5525
Ahmad <leonn_8m@hotmail.com>
2017-07-15 02:24:36 +0300

    Ok

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -28,7 +29,17 @@ app.get('/todos', (req, res)=> {
         res.status(400).send(e);
     })
 })
-
+app.get('/todos/:id', (req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) return res.status(404).send('Not a valid ID');
+    Todo.findById(id).then((todo)=>{
+        if(!todo) res.status(404).send('Todo not found');
+        res.send({todo});
+    }, (err)=>{
+        res.status(400).send('Not a valid ID');
+    });
+    
+});
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
