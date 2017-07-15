9f849841ec47519ddb7d192985ba68bbf3a9abd8
Ahmad <leonn_8m@hotmail.com>
2017-07-15 19:08:24 +0300

    Add full crud

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
 var port = process.env.PORT || 3000;
 var app = express();
 
@@ -49,6 +50,23 @@ app.delete('/todos/:id', (req,res)=>{
     },(err)=>{
         res.status(400).send('Invalid');
     })
+})
+
+app.patch('/todos/:id', (req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+        if(!todo) return res.status(404).send();
+        res.send({todo});
+    }).catch((e)=>res.status(404).send());
 })
 
 app.listen(port, () => {
